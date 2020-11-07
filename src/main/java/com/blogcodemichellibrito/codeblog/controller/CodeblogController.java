d9ce4e61e26df6c178c698f81dcdbaed01c3743f
= <=>
2020-11-07 10:23:47 -0300

    implementação do controller

---
+++ src/main/java/com/blogcodemichellibrito/codeblog/controller/CodeblogController.java
@@ -0,0 +1,27 @@
+package com.blogcodemichellibrito.codeblog.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.blogcodemichellibrito.codeblog.model.Post;
+import com.blogcodemichellibrito.codeblog.service.CodeblogService;
+
+@Controller
+public class CodeblogController {
+
+	@Autowired
+	CodeblogService service; //esta referenciando a interface e não a implementação, caso no futuro haja mais de uma implementação evitará o codigo quebrar
+	
+	@RequestMapping(value = "/posts", method = RequestMethod.GET)
+	public ModelAndView getPost() {
+		ModelAndView mv = new ModelAndView("posts");
+		List<Post> posts = service.findAll();
+		mv.addObject("posts", posts);
+		return mv;
+	}
+}
