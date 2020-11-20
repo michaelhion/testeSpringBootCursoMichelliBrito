d3c3f72c6b3d192bf08e68c77380235338d77ded
= <=>
2020-11-20 09:57:48 -0300

    criado template de novos posts

--- src/main/java/com/blogcodemichellibrito/codeblog/controller/CodeblogController.java
+++ src/main/java/com/blogcodemichellibrito/codeblog/controller/CodeblogController.java
@@ -1,13 +1,18 @@
 package com.blogcodemichellibrito.codeblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.blogcodemichellibrito.codeblog.model.Post;
 import com.blogcodemichellibrito.codeblog.service.CodeblogService;
@@ -32,5 +37,20 @@ public class CodeblogController {
 		Post post = service.findById(id);
 		mv.addObject("post", post);
 		return mv;
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		service.save(post);
+		return "redirect:/posts";
 	}
 }
